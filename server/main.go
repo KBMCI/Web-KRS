6588b71e41054b80c63ab0b72ccf23ac8f65c1ed
MuhammadIrtada <muhammadirtada@gmail.com>
2023-02-23 13:46:17 +0700

    add example data

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"time"
 	"web-krs/entity"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,7 +31,29 @@ func main() {
 		CreatedAt:  time.Now(),
 		UpdatedAt:  time.Now(),
 	}
-	kelas := [][]entity.Kelas{{ a,b }}
+	c := entity.Kelas{
+		Id:         3,
+		NamaKelas:  "C",
+		Hari:       "Rabu",
+		JamMulai:   "07.00",
+		JamSelesai: "07.49",
+		RuangKelas: "G1.2",
+		Matkul:     "Jaringan Komputer Dasar",
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+	d := entity.Kelas{
+		Id:         4,
+		NamaKelas:  "D",
+		Hari:       "Kamis",
+		JamMulai:   "07.00",
+		JamSelesai: "07.49",
+		RuangKelas: "G1.2",
+		Matkul:     "Jaringan Komputer Dasar",
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+	kelas := [][]entity.Kelas{{ a, b }, {c, d}}
 
 	app := gin.Default()
 
