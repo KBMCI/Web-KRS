216a22c3507c915562f1b3f92b54e54cc4749f20
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2023-03-03 14:24:03 +0700

    edit cors

--- server/app.go
+++ server/app.go
@@ -26,6 +26,13 @@ type (
 func InitServer(cfg config.Config) Server {
 	r := gin.Default()
 	
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	return &server{
 		httpServer: r,
 		cfg:		cfg,
@@ -34,7 +41,7 @@ func InitServer(cfg config.Config) Server {
 
 func(s *server) Run() {
 
-	s.httpServer.GET("/test", func(c *gin.Context) {
+	s.httpServer.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 		  "message": "test success",
 		})
@@ -45,13 +52,6 @@ func(s *server) Run() {
 	matkulHandler := handler.NewMatkulHandler(matkulService)
 	matkulGroup := s.httpServer.Group("/matkul")
 	matkulHandler.Mount(matkulGroup)
-
-	s.httpServer.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
-		AllowCredentials: true,
-	}))
 
 	if err := s.httpServer.Run(); err != nil {
 		log.Fatal(err)
