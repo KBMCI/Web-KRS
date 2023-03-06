4bb82e01a3eb74f443a8c2d78f6fea4973d04d51
Aldy Imam Wijaya <aldyimam03@gmail.com>
2023-03-06 22:39:16 +0700

    Update user.go

--- server/request/user.go
+++ server/request/user.go
@@ -2,10 +2,10 @@ package request
 
 type UserRequest struct {
 	ID           int    `json:"id"`
-	Email        string `json:"email" binding:"required,email" gorm:"unique;notnull"`
-	Nama         string `json:"nama" binding:"required"`
-	ProgramStudi string `json:"program_studi" binding:"required"`
-	Nim          string `json:"nim" binding:"required"`
-	Password     string `json:"password" binding:"required"`
-	Role         string `json:"role" binding:"required"`
+	Email        string `json:"email" binding:"email" gorm:"unique;notnull"`
+	Nama         string `json:"nama" gorm:"type:varchar(100)"`
+	ProgramStudi string `json:"program_studi" gorm:"type:varchar(30)"`
+	Nim          string `json:"nim" gorm:"type:varchar(20)"`
+	Password     string `json:"password" gorm:"type:varchar(100)"`
+	Role         string `json:"role" gorm:"type:varchar(5)"`
 }
