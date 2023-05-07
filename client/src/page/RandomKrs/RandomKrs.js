490650814dc69f903cdf38fb66344f147cb556ea
RazzaaqZul <112380919+RazzaaqZul@users.noreply.github.com>
2023-05-07 10:04:21 +0700

    meringkas TablePlan dan tambah TableHeader.js
    Membuat code menjadi lebih ringkas (TablePlan.js) dan menambahkan TableHeader untuk keperluan keterangan pada Table

--- client/src/page/RandomKrs/RandomKrs.js
+++ client/src/page/RandomKrs/RandomKrs.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import TablePlan from "./TablePlan";
+import { FiFilter } from "react-icons/fi";
 import Button from "../../component/Button";
 import Paginate from "../../component/Paginate";
-import { FiFilter, FiPlus } from "react-icons/fi";
-
+import TablePlan from "./TablePlan";
 
 const RandomKrs = () => {
   const [matkul, setMatkul] = useState([]);
@@ -24,26 +23,22 @@ const RandomKrs = () => {
 
   return (
     <>
-     <div className=" bg-secondary px-7 pt-7">
-      <div className="flex flex-row justify-between items-center">
-        <h3 className="text-sm font-semibold mb-4 pt-4">Kamu dapat menemukan Plan KRS tanpa melakukan perencanaan KRS secara manual.</h3>
-        <Button icon={<FiFilter />} name="Filter"/>
+      <div className=" bg-secondary px-7 pt-7">
+        <div className="flex flex-row justify-between items-center">
+          <h3 className="text-sm font-semibold mb-4 pt-4">
+            Kamu dapat menemukan Plan KRS tanpa melakukan perencanaan KRS secara
+            manual.
+          </h3>
+          <Button icon={<FiFilter />} name="Filter" />
+        </div>
       </div>
-      </div>
-      <TablePlan data={matkul[0]}></TablePlan>
+      <TablePlan data={matkul[1]}></TablePlan>
       <TablePlan data={matkul[2]}></TablePlan>
       <TablePlan data={matkul[3]}></TablePlan>
       <div className=" bg-secondary p-7">
-        
-
-          <Paginate
-      postPerPage={5}
-      totalPost={25}
-      paginate={Paginate}
-      />
+        <Paginate postPerPage={5} totalPost={25} paginate={Paginate} />
       </div>
     </>
-
   );
 };
 
--- client/src/page/RandomKrs/TablePlan.js
+++ client/src/page/RandomKrs/TablePlan.js
@@ -1,126 +1,15 @@
 import { useEffect, useState } from "react";
-import { header } from "./TableHeader";
+import { FiPlus } from "react-icons/fi";
 import Button from "../../component/Button";
-import Paginate from "../../component/Paginate";
-import { FiFilter, FiPlus } from "react-icons/fi";
-
+import { clear, header } from "./TableHeader";
 
 const TablePlan = ({ data }) => {
-  const [jadwalKuliah, setJadwalKuliah] = useState([
-    {
-      jam: "07.00 - 08.39",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "07.50 - 08.39",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "08.40 - 10.19",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "09.30 - 11.59",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "10.20 - 11.59",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "10.30 - 12.09",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "11.20 - 12.09",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "12.50 - 15.19",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "13.00 - 13.49",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "13.00 - 14.49",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "14.30 - 16.19",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "14.45 - 16.24",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "16.30 - 18.09",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-    {
-      jam: "17.20 - 18.09",
-      senin: "",
-      selasa: "",
-      rabu: "",
-      kamis: "",
-      jumat: "",
-    },
-  ]);
-
+  // menetapkan agar header tidak akan berubah
+  Object.freeze(header);
+  // menyalin header untuk dimanipulasi
+  const newData = [...header];
+  const [jadwalKuliah, setJadwalKuliah] = useState(newData);
+  // membuat plan
   const setPlan = () => {
     Array.isArray(data) &&
       data.map((item, i) => (
@@ -177,50 +66,52 @@ const TablePlan = ({ data }) => {
   };
 
   const headerTabel = () => {
-    return "px-4 py-2"
-  }
+    return "px-4 py-2";
+  };
 
   return (
     <>
-    <div className="min-h-[448px] bg-secondary px-7 pb-7 pt-4">
-      <div className="mt-5 mb-9"> {/* mbungkus semua plan */}
-        
-        <> {/* isi tiap plan */}
-          <h1 className="text-2xl font-bold mb-4">Plan 1</h1>
-
-          <table className="table-fixed border-collapse border-b border-neutral-400 w-full drop-shadow-2xl rounded-2xl overflow-hidden">
-            <thead>
-              <tr className="bg-primary text-secondary"> 
-                <th className={headerTabel()}></th>
-                <th className={headerTabel()}>Senin</th>
-                <th className={headerTabel()}>Selasa</th>
-                <th className={headerTabel()}>Rabu</th>
-                <th className={headerTabel()}>Kamis</th>
-                <th className={headerTabel()}>Jumat</th>
-              </tr>
-            </thead>
-            <tbody>
-              {jadwalKuliah.map((jadwal, index) => (
-                <tr key={index}
-                    className="bg-secondary text-neutral-900 border-b border-neutral-400">
-                  <td className={jamTabel()}>{jadwal.jam}</td>
-                  <td className={barisTabel()}>{jadwal.senin}</td>
-                  <td className={barisTabel()}>{jadwal.selasa}</td>
-                  <td className={barisTabel()}>{jadwal.rabu}</td>
-                  <td className={barisTabel()}>{jadwal.kamis}</td>
-                  <td className={barisTabel()}>{jadwal.jumat}</td>
+      <div className="min-h-[448px] bg-secondary px-7 pb-7 pt-4">
+        <div className="mt-5 mb-9">
+          {" "}
+          {/* mbungkus semua plan */}
+          <>
+            {" "}
+            {/* isi tiap plan */}
+            <h1 className="text-2xl font-bold mb-4">Plan 1</h1>
+            <table className="table-fixed border-collapse border-b border-neutral-400 w-full drop-shadow-2xl rounded-2xl overflow-hidden">
+              <thead>
+                <tr className="bg-primary text-secondary">
+                  <th className={headerTabel()}></th>
+                  <th className={headerTabel()}>Senin</th>
+                  <th className={headerTabel()}>Selasa</th>
+                  <th className={headerTabel()}>Rabu</th>
+                  <th className={headerTabel()}>Kamis</th>
+                  <th className={headerTabel()}>Jumat</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="my-6 flex justify-end">
-            <Button icon={<FiPlus/>} name="Add to &quot;MyPlans&quot;" />
-          </div>
-        </>
+              </thead>
+              <tbody>
+                {jadwalKuliah.map((jadwal, index) => (
+                  <tr
+                    key={index}
+                    className="bg-secondary text-neutral-900 border-b border-neutral-400">
+                    <td className={jamTabel()}>{jadwal.jam}</td>
+                    <td className={barisTabel()}>{jadwal.senin}</td>
+                    <td className={barisTabel()}>{jadwal.selasa}</td>
+                    <td className={barisTabel()}>{jadwal.rabu}</td>
+                    <td className={barisTabel()}>{jadwal.kamis}</td>
+                    <td className={barisTabel()}>{jadwal.jumat}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="my-6 flex justify-end">
+              <Button icon={<FiPlus />} name='Add to "MyPlans"' />
+            </div>
+          </>
+        </div>
       </div>
-      
-    </div>
-  </>
+    </>
   );
 };
 
