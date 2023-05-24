1f5a96c102f849a7787e8eb4d83b80b53aac2c23
MuhammadIrtada <88333614+MuhammadIrtada@users.noreply.github.com>
2023-05-24 09:04:48 +0700

    Update helper.go : change format hour

--- server/helper/helper.go
+++ server/helper/helper.go
@@ -6,7 +6,7 @@ import (
 )
 
 func StringToHours(jam string) float64{
-	hour, _ := time.ParseDuration(strings.Split(jam, ".")[0] + "h" + strings.Split(jam, ".")[1] + "m")
+	hour, _ := time.ParseDuration(strings.Split(jam, ":")[0] + "h" + strings.Split(jam, ":")[1] + "m")
 	return hour.Hours()
 }
 
