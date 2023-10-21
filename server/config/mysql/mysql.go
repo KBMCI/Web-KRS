02f8529c56faea048f75dfa2cdf6f9c2b8da03e3
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2023-10-21 22:37:33 +0700

    add model fakultas and prodi in migration

--- server/config/mysql/mysql.go
+++ server/config/mysql/mysql.go
@@ -9,25 +9,30 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitGorm(username, password, host, database string, port int, ) *gorm.DB {
+func InitGorm(username, password, host, database string, port int) *gorm.DB {
 	dsn := fmt.Sprintf(
-			"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", 
-			username, 
-			password, 
-			host, 
-			port, 
-			database,
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database,
 	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Cannot connect to database")
 	}
-	db.AutoMigrate(&model.User{}, &model.Matkul{}, &model.Kelas{}, &model.JadwalKelas{}, &model.JamKelas{}, &model.Plan{})
+	db.AutoMigrate(
+		&model.User{},
+		&model.Matkul{},
+		&model.Kelas{},
+		&model.JadwalKelas{},
+		&model.JamKelas{},
+		&model.Plan{},
+		&model.Fakultas{},
+		&model.ProgramStudi{},
+	)
 
 	return db
-  }
-
-  
-
-
+}
