3aecfac4afb55349b63e6788ae96db5975671378
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2023-10-21 22:39:50 +0700

    delete updateMatkulRequest

--- server/request/matkul.go
+++ server/request/matkul.go
@@ -7,11 +7,4 @@ type (
 		TahunKurikulum int16  `json:"tahun_kurikulum" binding:"required,number"`
 		Sks            int8   `json:"sks" binding:"required,number"`
 	}
-
-	UpdateMatkulRequest struct {
-		KodeMatkul     string `json:"kode_matkul"`
-		Nama           string `json:"nama" binding:"required"`
-		TahunKurikulum int16  `json:"tahun_kurikulum" binding:"required,number"`
-		Sks            int8   `json:"sks" binding:"required,number"`
-	}
 )
