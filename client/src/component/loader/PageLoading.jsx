9e4a29f1b7cdbc3dd64d0bf841f5d698edf065bd
Gazee <100186961+mahdabagas@users.noreply.github.com>
2024-01-13 15:31:58 +0700

    edit:  padiding in page loading

--- client/src/component/loader/PageLoading.jsx
+++ client/src/component/loader/PageLoading.jsx
@@ -2,8 +2,8 @@ import ReactLoading from "react-loading";
 
 const PageLoading = () => {
   return (
-    <section className="w-full h-full pt-10 flex justify-center items-center bg-white absolute top-1/2 -translate-y-1/2">
-        <ReactLoading type="spin" height={60} width={60} color="#4071F0" />
+    <section className="w-full h-full pt-8 flex justify-center items-center bg-white">
+      <ReactLoading type="spin" height={60} width={60} color="#4071F0" />
     </section>
   );
 };
