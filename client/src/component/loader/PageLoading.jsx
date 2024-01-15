3befd44374a74c0d80428aa9e7cee02b80c3ce2c
Gazee <100186961+mahdabagas@users.noreply.github.com>
2024-01-15 13:49:32 +0700

    edit: tampilan loading

--- client/src/component/loader/PageLoading.jsx
+++ client/src/component/loader/PageLoading.jsx
@@ -1,9 +1,11 @@
-import ReactLoading from "react-loading";
+import { AiOutlineLoading } from "react-icons/ai";
 
 const PageLoading = () => {
   return (
-    <section className="w-full h-full pt-8 flex justify-center items-center bg-white">
-      <ReactLoading type="spin" height={60} width={60} color="#4071F0" />
+    <section className="w-full  pt-8 flex justify-center items-center ">
+      <div className="animate-spin duration-50">
+        <AiOutlineLoading strokeWidth={10} size={64} color="4071F0" />
+      </div>
     </section>
   );
 };
