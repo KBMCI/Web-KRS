2a7cdc0e78e8793d1dfc3ed748ad1a0289129fa3
Gazee <100186961+mahdabagas@users.noreply.github.com>
2024-01-15 13:51:04 +0700

    edit: add loading, message

--- client/src/page/MyPlan/MyPlan.js
+++ client/src/page/MyPlan/MyPlan.js
@@ -2,21 +2,33 @@ import { useEffect, useState } from "react";
 import { url } from "../../api/url";
 import Paginations from "../../component/Paginations";
 import TablePlan from "../../component/TablePlan";
+import Message from "../PlanningKrs/components/message/Message";
+import PopUpDel from "../../component/PopUpDel";
+import PageLoading from "../../component/loader/PageLoading";
 
 const MyPlan = () => {
   const [myPlan, setMyPlan] = useState([]);
-  const [success, setSuccess] = useState(false);
 
   // Pagination Logic
   const [currentPage, setCurrentPage] = useState(1);
   const [postPerPage] = useState(5);
   const lastPostIndex = currentPage * postPerPage;
   const firstPostIndex = lastPostIndex - postPerPage;
-  const [displayPagination, setDisplayPagination] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [notif, setNotif] = useState({
+    open: false,
+    status: false,
+    message: "",
+  });
+
+  const [trigger, setTrigger] = useState(false);
+  const [loadingPage, setLoadingPage] = useState(true);
 
   // Mencoba get myplan
   useEffect(() => {
     const fetchMyPlan = async () => {
+      setLoadingPage(() => true);
       try {
         const token = localStorage.getItem("Authorization");
         let config = {
@@ -24,42 +36,107 @@ const MyPlan = () => {
             Authorization: `Bearer ${token}`,
           },
         };
-        console.log("Get Data My Plan");
-        const res = await url.get("/my-plan", config);
-        console.log(res.data.data);
-        setMyPlan(res.data.data);
-        setSuccess(true);
+        const result = await url.get("/my-plan", config);
+        if (result?.response?.data) {
+          setLoadingPage(() => false);
+        }
+
+        setMyPlan(result.data.data);
+
+        setTimeout(() => {
+          setLoadingPage(() => false);
+        }, 1000);
       } catch (err) {
         console.log(err);
       }
     };
     fetchMyPlan();
-  }, []);
+  }, [trigger]);
 
-  // Delete Handler
-  const deleteHandler = async (id) => {
-    console.log(id);
-    try {
-      const token = localStorage.getItem("Authorization");
-      let config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      console.log("Hapus Data");
-      const res = await url.delete(`/my-plan/${id}`, config);
-      console.log(res);
-      if (res.status === 200) {
-        window.location.reload();
+  //  create delete show
+  const [showDel, setShowDel] = useState({
+    show: false,
+    id: null,
+  });
+
+  // perform deletion and hide popup
+  const deleteHandlerTrue = async () => {
+    if (showDel.show && showDel.id) {
+      setLoading(true);
+      try {
+        const token = localStorage.getItem("Authorization");
+        let config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const result = await url.delete(`/my-plan/${showDel.id}`, config);
+
+        if (result?.response?.data) {
+          setNotif(() => ({
+            open: true,
+            status: false,
+            message: result.response.data.message,
+          }));
+
+          setLoading(false);
+
+          setTimeout(() => {
+            setNotif((prev) => ({
+              ...prev,
+              open: false,
+            }));
+          }, 2000);
+          return;
+        }
+
+        setNotif(() => ({
+          open: true,
+          status: true,
+          message: result.data.message,
+        }));
+        setLoading(false);
+
+        setTrigger((prev) => !prev);
+
+        setShowDel(() => ({
+          show: false,
+          id: null,
+        }));
+
+        setTimeout(() => {
+          setNotif((prev) => ({
+            ...prev,
+            open: false,
+          }));
+        }, 2000);
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
     }
+  };
+
+  // Show Confimation Box Delete
+  const deleteHandler = (id) => {
+    setShowDel({
+      show: true,
+      id,
+    });
+  };
+
+  // hide confrimation box
+  const deleteHandlerFalse = () => {
+    setShowDel({
+      show: false,
+      id: null,
+    });
   };
 
   return (
     <>
-      {success && (
+      {loadingPage ? (
+        <PageLoading />
+      ) : (
         <>
           <div className=" bg-secondary px-7 pt-7">
             <div className="flex flex-row justify-between items-center">
@@ -82,6 +159,7 @@ const MyPlan = () => {
                     deleteHandler={deleteHandler}
                     idDelete={plans.id}
                     idPlan={plans.id}
+                    loading={loading}
                   />
                 </div>
               );
@@ -95,8 +173,22 @@ const MyPlan = () => {
               currentPage={currentPage}
             />
           </div>
+
+          {showDel.show && (
+            <PopUpDel
+              deleteHandlerFalse={deleteHandlerFalse}
+              deleteHandlerTrue={deleteHandlerTrue}
+              loading={loading}
+              name={"plan"}
+            />
+          )}
         </>
       )}
+      <Message
+        open={notif.open}
+        statusMsg={notif.status}
+        textMsg={notif.message}
+      />
     </>
   );
 };
