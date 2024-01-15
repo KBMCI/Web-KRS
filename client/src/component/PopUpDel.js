b3100de49960e3f86df24761600438b160f9ae01
Gazee <100186961+mahdabagas@users.noreply.github.com>
2024-01-15 13:53:56 +0700

    edit: component popup

--- client/src/component/PopUpDel.js
+++ client/src/component/PopUpDel.js
@@ -1,7 +1,13 @@
 import { FiAlertTriangle } from "react-icons/fi";
 import ReactLoading from "react-loading";
+import Button from "./button/Button";
 
-export default function PopUpDel({ deleteHandlerFalse, deleteHandlerTrue, loading }) {
+export default function PopUpDel({
+  deleteHandlerFalse,
+  deleteHandlerTrue,
+  loading,
+  name,
+}) {
   return (
     <div className="flex justify-center align-center fixed inset-0 z-50 bg-black/30 backdrop-blur-sm ">
       <div className="relative w-full max-w-sm my-auto bg-secondary px-[50px] py-5 rounded-[10px] ">
@@ -9,26 +15,25 @@ export default function PopUpDel({ deleteHandlerFalse, deleteHandlerTrue, loadin
         <div className="flex items-center flex-col gap-[10px]">
           <FiAlertTriangle size="70px" />
           <p className="font-semibold text-center">
-            Apakah Kamu yakin ingin mengubah data ini?
+            Apakah kamu yakin ingin menghapus {name ? name : "data"} ini?
           </p>
         </div>
         <div className="w-full flex justify-around gap-4 mt-5">
-          <button
-            type="button"
-            className="text-neutral-400 py-2 px-[44.5px] rounded-lg border-2 border-neutral-400 font-bold"
+          <Button
             onClick={deleteHandlerFalse}
+            className={`bg-secondary border-2 border-neutral-400 text-neutral-400 h-12 gap-0 w-full`}
           >
             Cancel
-          </button>
-          <button
-            className="bg-primary text-secondary py-2 px-[60px] rounded-lg font-bold text-base"
+          </Button>
+          <Button
             onClick={deleteHandlerTrue}
-            disabled={loading}
+            loading={loading}
+            className={`bg-primary text-secondary h-12 gap-0 w-full`}
           >
-            {loading ? <ReactLoading height="5px" width="14px" /> : "OK"}
-          </button>
+            OK
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
